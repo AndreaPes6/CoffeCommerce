cf1703ecf45e7a413c7de2205b961f0f1f90cb2e
ramonasecci <ramonasecci@live.it>
2024-02-19 20:47:51 +0100

    Aggiunto foglio di stile, cartella con logo e modificato stile nella Home

--- ContentShop/HomeShop.aspx
+++ ContentShop/HomeShop.aspx
@@ -1,13 +1,11 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Templates/TemplateShop.master" AutoEventWireup="true" CodeBehind="HomeShop.aspx.cs" Inherits="CoffeCommerce.ContentShop.HomeShop" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <!-- Contenuto della tua pagina principale -->
+    <div class="container-lg w-75 m-auto my-5">
+        <h5 style="margin-top: 100px;">Explore</h5>
+        <div id="contentHtml" runat="server" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-3">
+        </div>
 
-    <div>
-        <h2>Benvenuto</h2>
-        <p>Qui puoi trovare una selezione dei migliori prodotti.</p>
     </div>
-
-    <!-- Aggiungi ulteriori controlli o HTML come necessario -->
 
 </asp:Content>
--- ContentShop/HomeShop.aspx.cs
+++ ContentShop/HomeShop.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,58 @@ namespace CoffeCommerce.ContentShop
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
+                DBConn.conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Products", DBConn.conn);
+                SqlDataReader dataReader = cmd.ExecuteReader();
 
+                string content = "";
+
+                if (dataReader.HasRows)
+                {
+                    while (dataReader.Read())
+                    {
+                        content += $@"<div class=""col"">
+                                       <div class=""c-card card h-100 d-flex flex-column justify-content-between border border-0"">
+                                                                 <div class=""p-2 d-flex justify-content-center"">
+                                              <img src=""{dataReader["FotoProduct"]}"" class=""card-img-top rounded"" alt=""{dataReader["Name"]}"" style=""width:150px; height:auto;"">
+                                          
+                                                                </div>
+                                             <div>
+                                                 <div class=""card-body "">
+                                              <h6 class=""card-title fw-bolder"">{dataReader["Name"]}</h6>
+                                              <p class=""card-text  text-secondary"" style=""font-size:0.8em;"">{dataReader["Description"]}</p>   
+                                           </div>
+                                             <div class="" d-flex justify-content-around align-items-baseline p-3"">
+                                                
+                                               <p class=""card-text me-2"">€{dataReader["Price"]}</p>
+                                               <a href=""DetailsShop.aspx?product={dataReader["ID"]}"" style=""font-size:0.7em;"" class=""btn btn-card rounded-pill"">Details</a>
+                                            </div>
+                                               </div>
+                                          
+                                          
+                                              
+                                           </div>
+                                      </div> ";
+
+                    }
+                }
+                contentHtml.InnerHtml = content;
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.ToString());
+            }
+            finally
+            {
+                if(DBConn.conn.State == System.Data.ConnectionState.Open)
+                {
+                    DBConn.conn.Close();
+                }
+
+            }
         }
     }
 }
