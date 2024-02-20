5b490d60bb20de3c4b87c271b81c040b05f8a1e2
Vincenzolaveglia <vincenzolaveglia10@gmail.com>
2024-02-20 15:57:58 +0100

    aggiunta logout

--- ContentShop/Cart.aspx.cs
+++ ContentShop/Cart.aspx.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+/*
 namespace CoffeCommerce.ContentShop
 {
    
@@ -81,4 +81,4 @@ namespace CoffeCommerce.ContentShop
             }
         }
     }
-}
+}*/
