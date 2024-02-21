57a80b83591b7f55b555735a3c84c1d387cf5baf
ramonasecci <ramonasecci@live.it>
2024-02-21 19:22:18 +0100

    add icon for cart in card home and edit position

--- ContentShop/HomeShop.aspx
+++ ContentShop/HomeShop.aspx
@@ -4,7 +4,7 @@
     <div class="container-lg w-75 m-auto my-5">
         <div class="d-flex justify-content-between align-items-center mb-5">
             <h5 id="tltCategory" runat="server">All Products</h5>
-            
+
             <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" AutoPostBack="true" CssClass="form-select dp-dw-cat">
                 <asp:ListItem Value="0">All Categories</asp:ListItem>
                 <asp:ListItem Value="1">Capsule Dolce Gusto</asp:ListItem>
@@ -13,7 +13,50 @@
                 <asp:ListItem Value="4">Cialde ESE</asp:ListItem>
             </asp:DropDownList>
         </div>
-        <div id="contentHtml" runat="server" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-3">
+        <div id="contentHtml" runat="server" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4  g-3">
+            <asp:Repeater ID="ProductRepeater" runat="server">
+                <ItemTemplate>
+                    <div class="col">
+                        <div class="c-card card h-100 d-flex flex-column justify-content-between">
+                            <div class="p-2 d-flex justify-content-center">
+                                <img src='<%# Eval("FotoProduct") %>' class="card-img-top rounded" alt='<%# Eval("Name") %>' style="width: 150px; height: auto;" />
+                            </div>
+                            <div>
+                                <div class="card-body text-center">
+                                    <h6 class="card-title "><%# Eval("Name") %></h6>
+                                    <p class="card-text text-secondary" style="font-size: 0.8em;"><%# Eval("Description") %></p>
+                                 
+                                </div>
+                                <div class="d-flex justify-content-around align-items-baseline p-3">
+                                       <p class="card-text me-2 fs-5 ">€ <%# Eval("Price") %></p>
+
+                                    <div class="d-flex justify-content-end align-items-center p-3">
+                                    <asp:LinkButton ID="LinkButton1" runat="server" OnClick="btnAddToCart_Click" CommandArgument='<%# Eval("ID") %>'>
+                                        <div class="d-flex justify-content-center text-white rounded-circle p-2 h-btn-cart">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16" >
+                                            <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9z" />
+                                            <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
+                                        </svg>
+                                            </div>
+
+                                    </asp:LinkButton>
+
+                                    <a href='<%# "DetailsShop.aspx?product=" + Eval("ID") %>' class="ms-1">
+                                        <div class="d-flex justify-content-center align-items-center btn-card rounded-circle p-1">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-info-lg " viewBox="0 0 16 16">
+                                                <path d="m9.708 6.075-3.024.379-.108.502.595.108c.387.093.464.232.38.619l-.975 4.577c-.255 1.183.14 1.74 1.067 1.74.72 0 1.554-.332 1.933-.789l.116-.549c-.263.232-.65.325-.905.325-.363 0-.494-.255-.402-.704zm.091-2.755a1.32 1.32 0 1 1-2.64 0 1.32 1.32 0 0 1 2.64 0" />
+                                            </svg>
+
+                                        </div>
+                                    </a>
+                                </div>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </ItemTemplate>
+            </asp:Repeater>
+
         </div>
     </div>
 </asp:Content>
--- ContentShop/HomeShop.aspx.cs
+++ ContentShop/HomeShop.aspx.cs
@@ -12,139 +12,25 @@ namespace CoffeCommerce.ContentShop
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                BindData();
+            }
+        }
+
+        private void BindData()
+        {
             try
             {
                 DBConn.conn.Open();
                 SqlCommand cmd = new SqlCommand("SELECT * FROM Products", DBConn.conn);
                 SqlDataReader dataReader = cmd.ExecuteReader();
-                tltCategory.InnerText = "All Category";
-
-                string content = "";
 
                 if (dataReader.HasRows)
                 {
-                    while (dataReader.Read())
-                    {
-                        content += $@"<div class=""col"">
-                                       <div class=""c-card card h-100 d-flex flex-column justify-content-between"">
-                                                                 <div class=""p-2 d-flex justify-content-center"">
-                                              <img src=""{dataReader["FotoProduct"]}"" class=""card-img-top rounded"" alt=""{dataReader["Name"]}"" style=""width:150px; height:auto;"">
-                                          
-                                                                </div>
-                                             <div>
-                                                 <div class=""card-body text-center"">
-                                              <h6 class=""card-title fw-bolder"">{dataReader["Name"]}</h6>
-                                              <p class=""card-text  text-secondary"" style=""font-size:0.8em;"">{dataReader["Description"]}</p>   
-                                           </div>
-                                             <div class="" d-flex justify-content-around align-items-baseline p-3"">
-                                                
-                                               <p class=""card-text me-2 fw-medium"">€ {dataReader["Price"]}</p>
-                                               <a href=""DetailsShop.aspx?product={dataReader["ID"]}""  class=""btn btn-card rounded-pill"">Details</a>
-                                            </div>
-                                               </div>
-                                          
-                                          
-                                              
-                                           </div>
-                                      </div> ";
-
-                    }
+                    ProductRepeater.DataSource = dataReader;
+                    ProductRepeater.DataBind();
                 }
-                contentHtml.InnerHtml = content;
-
-            }
-            catch (Exception ex)
-            {
-                Response.Write(ex.ToString());
-            }
-            finally
-            {
-                if(DBConn.conn.State == System.Data.ConnectionState.Open)
-                {
-                    DBConn.conn.Close();
-                }
-
-            }
-        }
-
-        //mostra in base alla categoria selezionata dalla dropdownlist
-        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            try
-            {
-                DBConn.conn.Open();
-
-                string content = "";
-                int IDSelected = int.Parse(DropDownList1.SelectedValue);
-
-                if (IDSelected == 0)
-                {
-                    // Se l'ID selezionato è 0 All Categories mostra tutti i tipi di prodotti
-                    SqlCommand cmdAllProducts = new SqlCommand("SELECT * FROM Products", DBConn.conn);
-                    SqlDataReader dataReader = cmdAllProducts.ExecuteReader();
-                    tltCategory.InnerText = "All Categories";
-
-                    if (dataReader.HasRows)
-                    {
-                        while (dataReader.Read())
-                        {
-                            content += $@"<div class=""col"">
-                                   <div class=""c-card card h-100 d-flex flex-column justify-content-between"">
-                                             <div class=""p-2 d-flex justify-content-center"">
-                                                  <img src=""{dataReader["FotoProduct"]}"" class=""card-img-top rounded"" alt=""{dataReader["Name"]}"" style=""width:150px; height:auto;"">
-                                             </div>
-                                             <div>
-                                                 <div class=""card-body text-center"">
-                                                      <h6 class=""card-title fw-bolder"">{dataReader["Name"]}</h6>
-                                                      <p class=""card-text  text-secondary"" style=""font-size:0.8em;"">{dataReader["Description"]}</p>   
-                                                 </div>
-                                                 <div class="" d-flex justify-content-around align-items-baseline p-3"">
-                                                     <p class=""card-text me-2 fw-medium"">€ {dataReader["Price"]}</p>
-                                                     <a href=""DetailsShop.aspx?product={dataReader["ID"]}""  class=""btn btn-card rounded-pill"">Details</a>
-                                                 </div>
-                                             </div>
-                                        </div>
-                                  </div> ";
-                        }
-                    }
-
-                    dataReader.Close();
-                }
-                else
-                {
-                    // Altrimenti mostra i prodotti della categoria selezionata della drop
-                    SqlCommand cmd = new SqlCommand($"SELECT * FROM Products WHERE IDCategory = {IDSelected}", DBConn.conn);
-                    SqlDataReader dataReader = cmd.ExecuteReader();
-
-                    if (dataReader.HasRows)
-                    {
-                        while (dataReader.Read())
-                        {
-                            content += $@"<div class=""col"">
-                                   <div class=""c-card card h-100 d-flex flex-column justify-content-between"">
-                                             <div class=""p-2 d-flex justify-content-center"">
-                                                  <img src=""{dataReader["FotoProduct"]}"" class=""card-img-top rounded"" alt=""{dataReader["Name"]}"" style=""width:150px; height:auto;"">
-                                             </div>
-                                             <div>
-                                                 <div class=""card-body text-center"">
-                                                      <h6 class=""card-title fw-bolder"">{dataReader["Name"]}</h6>
-                                                      <p class=""card-text  text-secondary"" style=""font-size:0.8em;"">{dataReader["Description"]}</p>   
-                                                 </div>
-                                                 <div class="" d-flex justify-content-around align-items-baseline p-3"">
-                                                     <p class=""card-text me-2 fw-medium"">€ {dataReader["Price"]}</p>
-                                                     <a href=""DetailsShop.aspx?product={dataReader["ID"]}""  class=""btn btn-card rounded-pill"">Details</a>
-                                                 </div>
-                                             </div>
-                                        </div>
-                                  </div> ";
-                        }
-                    }
-
-                    dataReader.Close();
-                    tltCategory.InnerText = DropDownList1.SelectedItem.Text;
-                }
-
-                contentHtml.InnerHtml = content;
             }
             catch (Exception ex)
             {
@@ -158,6 +44,83 @@ namespace CoffeCommerce.ContentShop
                 }
             }
         }
+
+        //mostra in base alla categoria selezionata dalla dropdownlist
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+{
+    try
+    {
+        DBConn.conn.Open();
+        int IDSelected = int.Parse(DropDownList1.SelectedValue);
+        string query = (IDSelected == 0) ? "SELECT * FROM Products" : $"SELECT * FROM Products WHERE IDCategory = {IDSelected}";
+
+        SqlCommand cmd = new SqlCommand(query, DBConn.conn);
+        SqlDataReader dataReader = cmd.ExecuteReader();
+
+        if (dataReader.HasRows)
+        {
+            ProductRepeater.DataSource = dataReader;
+            ProductRepeater.DataBind();
+        }
+
+        tltCategory.InnerText = (IDSelected == 0) ? "All Categories" : DropDownList1.SelectedItem.Text;
+    }
+    catch (Exception ex)
+    {
+        Response.Write(ex.ToString());
+    }
+    finally
+    {
+        if (DBConn.conn.State == System.Data.ConnectionState.Open)
+        {
+            DBConn.conn.Close();
+        }
+    }
+}
+
+        protected void btnAddToCart_Click(object sender, EventArgs e)
+        {
+            LinkButton btnAddToCart = (LinkButton)sender;
+            string productId = btnAddToCart.CommandArgument;
+            if (!string.IsNullOrEmpty(productId))
+            {
+                if (int.TryParse(productId, out int ProdID))
+                {
+                    int quantity = 1;
+
+                    CartItem item = new CartItem(ProdID, quantity);
+
+                    List<CartItem> products;
+                    if (Session["Carrello"] == null)
+                    {
+                        // Se la sessione non esiste, crea una nuova lista
+                        products = new List<CartItem>();
+                    }
+                    else
+                    {
+                        // Se la sessione esiste, recupera la lista esistente
+                        products = (List<CartItem>)Session["Carrello"];
+                    }
+
+                    // Aggiungi il nuovo elemento alla lista
+                    products.Add(item);
+
+                    // Aggiorna la sessione con la lista aggiornata
+                    Session["Carrello"] = products;
+
+                    ScriptManager.RegisterStartupScript(this, GetType(), "addToCartItem", "alert('Articolo aggiunto con successo al carrello!');", true);
+                }
+                else
+                {
+                    Response.Write("<p class='text-danger'>Invalid product ID format</p>");
+                }
+            }
+            else
+            {
+                Response.Write("<p class='text-danger'>Product ID is null or empty</p>");
+            }
+        }
+
 
     }
 }
