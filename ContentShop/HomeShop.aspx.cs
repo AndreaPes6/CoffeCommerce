35b2af570d20938ce908435221e7296fcc3ab658
ramonasecci <ramonasecci@live.it>
2024-02-21 23:51:42 +0100

    adattato ultimo push

--- ContentShop/HomeShop.aspx.cs
+++ ContentShop/HomeShop.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -22,41 +23,19 @@ namespace CoffeCommerce.ContentShop
         {
             try
             {
-                DBConn.conn.Open();
-                SqlCommand cmd = new SqlCommand("SELECT * FROM Products", DBConn.conn);
+                DBConn.conn.Open();            
+                string query =  "SELECT * FROM Products";
+
+                SqlCommand cmd = new SqlCommand(query, DBConn.conn);
                 SqlDataReader dataReader = cmd.ExecuteReader();
 
                 if (dataReader.HasRows)
                 {
                     ProductRepeater.DataSource = dataReader;
                     ProductRepeater.DataBind();
-                    while (dataReader.Read())
-                    {
-                        content += $@"<div class=""col"">
-                                       <div class=""c-card card h-100 d-flex flex-column justify-content-between"">
-                                                                 <div class=""p-2 d-flex justify-content-center"">
-                                              <img src=""{dataReader["FotoProduct"]}"" class=""card-img-top rounded"" alt=""{dataReader["Name"]}"" style=""width:150px; height:auto;"">
-                                          
-                                                                </div>
-                                             <div>
-                                                 <div class=""card-body text-center"">
-                                              <h6 class=""card-title fw-bolder"">{dataReader["Name"]}</h6>
-                                              <p class=""card-text text-truncate  text-secondary"" style=""font-size:0.8em;"">{dataReader["Description"]}</p>   
-                                           </div>
-                                             <div class="" d-flex justify-content-around align-items-baseline p-3"">
-                                                
-                                               <p class=""card-text me-2 fw-medium"">€ {dataReader["Price"]}</p>
-                                               <a href=""DetailsShop.aspx?product={dataReader["ID"]}""  class=""btn btn-card rounded-pill"">Details</a>
-                                            </div>
-                                               </div>
-                                          
-                                          
-                                              
-                                           </div>
-                                      </div> ";
-
-                    }
                 }
+
+                tltCategory.InnerText =  "All Categories";
             }
             catch (Exception ex)
             {
