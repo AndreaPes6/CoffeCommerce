13d19cd237ff0f2888176a70f85a2b8c7c778c63
Vincenzolaveglia <vincenzolaveglia10@gmail.com>
2024-02-22 15:41:45 +0100

    aggiustamenti editProfile e aggiunta deleteProfile

--- Profile/EditProfile.aspx.designer.cs
+++ Profile/EditProfile.aspx.designer.cs
@@ -69,6 +69,15 @@ namespace CoffeCommerce.Profile
         protected global::System.Web.UI.WebControls.TextBox txtPassword;
 
         /// <summary>
+        /// Controllo LblErrorMessage.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblErrorMessage;
+
+        /// <summary>
         /// Controllo lblImageProfile.
         /// </summary>
         /// <remarks>
