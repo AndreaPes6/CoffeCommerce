0eb292754865da2c6b437bf2745d2aac823630a7
MethaKinde <meethabeelkinde@gmail.com>
2024-02-23 11:23:40 +0100

    funzionalita dello switch tema

--- Assets/Scripts/JavaScript.js
+++ Assets/Scripts/JavaScript.js
@@ -1,6 +1,18 @@
-﻿const body = document.querySelector('body');
-const toggle = document.getElementsById('toggle');
+﻿console.log("ci sono")
+const body = document.querySelector('body');
+const toggle = document.getElementById('toggle');
+
+//toggle.onclick = function () {
+//    toggle.classList.add('active')
+//    body.classList.add('active')
+//}
+
+//toggle.addEventListener("click", function (e) {
+//    toggle.classList.add("active")
+//    body.classList.add("active")
+//})
+
 toggle.onclick = function () {
-    toggle.classList.add('active')
-    body.classList.add('active')
+    toggle.classList.toggle('active')
+    body.classList.toggle('active')
 }
